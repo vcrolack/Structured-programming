ee7e9e4589cfebbbf1e0636a0358e990d9b89385
Víctor Rolack <victorolack@gmail.com>
2020-07-01 00:02:10 -0400

    Declare variables

---
+++ basic-concepts/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int integerA = 1;
+    float floatA = 1.1;
+    char letterA = 'A';
+    bool boolean = false;
+
+    return 0;
+}
